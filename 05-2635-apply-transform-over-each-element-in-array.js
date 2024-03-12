55b04fc6a8f66009f3ed16362a3d4326aac40d7e
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-03-12 18:05:42 +0700

    05 completed - 2635. Apply Transform Over Each Element in Array

--- 05-2635-apply-transform-over-each-element-in-array.js
+++ 05-2635-apply-transform-over-each-element-in-array.js
@@ -3,11 +3,22 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-  const result = [];
 
-  for (const i in arr) {
-    result.push(fn(arr[i], Number(i)));
+var map = function(arr, fn) {
+  for(let i=0; i<arr.length; i++){
+      arr[i]=fn(arr[i], i);
   }
-  return result;
+
+  return arr;
 };
+
+// -----------------
+// Faster solution:
+// var map = function(arr, fn) {
+//   const result = [];
+
+//   for (const i in arr) {
+//     result.push(fn(arr[i], Number(i)));
+//   }
+//   return result;
+// };
