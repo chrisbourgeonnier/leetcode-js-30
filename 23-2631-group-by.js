843e61c2e9dbd61c9b84964799eb5163986dad30
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-03-28 11:16:45 +0700

    23 completed - 2631. Group By

---
+++ 23-2631-group-by.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+  const group = {};
+
+  for (const obj of this) {
+    const key = fn(obj);
+    group[key] = group[key] || [];
+    group[key].push(obj);
+  }
+  return group;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
